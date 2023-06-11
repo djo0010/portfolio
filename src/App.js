885770072d07de0686929216b8d378c7782eebda
DJ Oakman <djoakman512@gmail.com>
2023-06-11 14:01:33 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        
+      </div>  
+    </ThemeProvider>
+    
+  );
+}
+
+export default App;
